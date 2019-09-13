f2c9448ec4cf401890c093ed38b828a0bd828df5
Jing <jinghu0923@gmail.com>
2019-09-13 18:09:06 +0000

    add start point before deploy to heroku

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ app.post("/register", function(req,res){
 			return res.render("register", {error:err.message});
 		}
 		passport.authenticate("local")(req, res, function(){
-										req.flash("success", "Welcome to Purrfect Match!" + user.username);
+										req.flash("success", "Welcome to Purrfect Match! " + user.username);
 									   res.redirect("/cat");
 									   });
 	});
